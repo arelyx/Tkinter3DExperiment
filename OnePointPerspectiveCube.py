92bfbffb98823b1132ba0c80696b36e7cb4d905e
nyt <jonsspamaccout@gmail.com>
2018-07-02 23:32:45 -0700

    allow screen resizing

--- OnePointPerspectiveCube.py
+++ OnePointPerspectiveCube.py
@@ -15,28 +15,35 @@ class PerspectiveCube:
             p = canvas.create_polygon(0,0,0,0,0,0, outline=color, fill='', width=4)
             self.canvas.tag_bind(p, "<B1-Motion>", self._on_clickndrag)
             self.polygons.append(p)
-        self.canvas.tag_bind(self.polygons, "<B1-Motion>", self._on_clickndrag)
-        self.update_screen(randint(50, 550), randint(50, 550)) #initial point
+        self.update_screen(
+            randint(50, canvas.winfo_width()),
+            randint(50, canvas.winfo_height())) #initial point
 
     def _on_clickndrag(self, event):
         self.update_screen(event.x, event.y)
 
-    def update_screen(self, x, y):
+    def update_screen(self, x=None, y=None):
         # A HORRIBLE MESS OF VARIABLES. DON'T LOOK. IT MIGHT BURN YOUR EYES.
-        x1 = x-50
-        y1 = y-50
-        x2 = x+50
-        y2 = y+50
-        x3 = (300+x1)/2
-        y3 = (300+y2)/2
-        x4 = (300+x1+100)/2
-        y4 = (300+y2)/2
-        x5 = (300+x1)/2
-        y5 = (300+y1)/2
-        x6 = (300+x1)/2
-        y6 = (300+y2)/2
-        x7 = (300+x1+100)/2
-        y7 = (300+y1)/2
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+        half_width = self.canvas.winfo_width() / 2
+        half_height = self.canvas.winfo_height() / 2
+        x1 = self.x-50
+        y1 = self.y-50
+        x2 = self.x+50
+        y2 = self.y+50
+        x3 = (half_width+x1)/2
+        y3 = (half_height+y2)/2
+        x4 = (half_width+x1+100)/2
+        y4 = (half_height+y2)/2
+        x5 = (half_width+x1)/2
+        y5 = (half_height+y1)/2
+        x6 = (half_width+x1)/2
+        y6 = (half_height+y2)/2
+        x7 = (half_width+x1+100)/2
+        y7 = (half_height+y1)/2
 
         self.canvas.coords(self.polygons[0], x3, y3, x4, y4, x2, y2, x2 - 100, y2)
         self.canvas.coords(self.polygons[1], x5, y5, x6, y6, x2 - 100, y2, x1, y1)
@@ -48,8 +55,9 @@ class PerspectiveCanvas(tk.Frame):
         tk.Frame.__init__(self, master, bg='black', **kwargs)
         label = tk.Label(self, text = "Click and Drag to See the Shape",background='black',foreground='white')
         label.pack()
-        self.canvas = tk.Canvas(self, width=600, height=600, bg='black')
-        self.canvas.pack()
+        self.canvas = tk.Canvas(self, bg='black')
+        self.canvas.pack(fill=tk.BOTH, expand=True)
+        self.canvas.bind('<Configure>', self.update_all)
         btn = ttk.Button(self, text='add polygon', command=self.add)
         btn.place(relx=1, rely=0, anchor='ne')
         self.cubes = []
@@ -57,12 +65,17 @@ class PerspectiveCanvas(tk.Frame):
     def add(self):
         self.cubes.append(PerspectiveCube(self.canvas))
 
+    def update_all(self, event):
+        for cube in self.cubes:
+            cube.update_screen()
+
 def main():
     root = tk.Tk()
     root.title("OnePointPerspectiveCube")
     root.configure(background='black')
+    root.geometry('600x600')
     win = PerspectiveCanvas(root)
-    win.pack()
+    win.pack(fill=tk.BOTH, expand=True)
     root.mainloop()
 
 if __name__ == '__main__':
