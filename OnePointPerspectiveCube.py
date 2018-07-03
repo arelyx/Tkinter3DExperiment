49f8bc4909f704a281c52a3d94f8fb2929b0e94e
nyt <jonsspamaccout@gmail.com>
2018-07-02 22:57:34 -0700

    convert to OOP

--- OnePointPerspectiveCube.py
+++ OnePointPerspectiveCube.py
@@ -1,39 +1,42 @@
+#!/usr/bin/env python3
+
 from tkinter import *
 
-def shape(event):
-    global line_one, line_two, line_three, line_four, polygon_bottom, polygon_top, polygon_left, polygon_right
-    canvas.delete(line_one, line_two, line_three, line_four,polygon_bottom, polygon_top, polygon_left, polygon_right)
+class PerspectiveCube:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.polygons = []
+        for _ in range(4):
+            p = canvas.create_polygon(0,0,0,0,0,0, outline='red', fill='', width=4)
+            self.canvas.tag_bind(p, "<B1-Motion>", self._on_clickndrag)
+            self.polygons.append(p)
+        self.canvas.tag_bind(self.polygons, "<B1-Motion>", self._on_clickndrag)
+        self.update_screen(200, 200) #initial point
 
-    # A HORRIBLE MESS OF VARIABLES. DON'T LOOK. IT MIGHT BURN YOUR EYES.
-    x = event.x
-    y = event.y
-    x1 = x-50
-    y1 = y-50
-    x2 = x+50
-    y2 = y+50
-    x3 = (300+x1)/2
-    y3 = (300+y2)/2
-    x4 = (300+x1+100)/2
-    y4 = (300+y2)/2
-    x5 = (300+x1)/2
-    y5 = (300+y1)/2
-    x6 = (300+x1)/2
-    y6 = (300+y2)/2
-    x7 = (300+x1+100)/2
-    y7 = (300+y1)/2
+    def _on_clickndrag(self, event):
+        self.update_screen(event.x, event.y)
 
+    def update_screen(self, x, y):
+        # A HORRIBLE MESS OF VARIABLES. DON'T LOOK. IT MIGHT BURN YOUR EYES.
+        x1 = x-50
+        y1 = y-50
+        x2 = x+50
+        y2 = y+50
+        x3 = (300+x1)/2
+        y3 = (300+y2)/2
+        x4 = (300+x1+100)/2
+        y4 = (300+y2)/2
+        x5 = (300+x1)/2
+        y5 = (300+y1)/2
+        x6 = (300+x1)/2
+        y6 = (300+y2)/2
+        x7 = (300+x1+100)/2
+        y7 = (300+y1)/2
 
-    # UNCOMMENT FOR A FREE COOKIE!
-    # line_one = canvas.create_line(300,300,x1,y1, fill='white')
-    # line_two = canvas.create_line(300,300,x1+100,y1, fill='white')
-    # line_three = canvas.create_line(300,300,x2-100,y2, fill='white')
-    # line_four = canvas.create_line(300,300,x2,y2, fill='white')
-
-    polygon_bottom = canvas.create_polygon(x3, y3, x4, y4, x2, y2, x2 - 100, y2, outline='red', fill='', width=4)
-    polygon_left = canvas.create_polygon(x5, y5, x6, y6, x2 - 100, y2, x1, y1, outline='red', fill='', width=4)
-    polygon_top = canvas.create_polygon(x5, y5, x1, y1, x1+100, y1, x7, y7, outline='red', fill='', width=4)
-    polygon_right= canvas.create_polygon(x2,y2,x1+100,y1,x7,y7, x4, y4, outline='red', fill='', width=4)
-
+        self.canvas.coords(self.polygons[0], x3, y3, x4, y4, x2, y2, x2 - 100, y2)
+        self.canvas.coords(self.polygons[1], x5, y5, x6, y6, x2 - 100, y2, x1, y1)
+        self.canvas.coords(self.polygons[2], x5, y5, x1, y1, x1+100, y1, x7, y7)
+        self.canvas.coords(self.polygons[3], x2,y2,x1+100,y1,x7,y7, x4, y4)
 
 tk = Tk()
 tk.title("OnePointPerspectiveCube")
@@ -42,16 +45,6 @@ label = Label(text = "Click and Drag to See the Shape",background='black',foregr
 label.pack()
 canvas = Canvas(width=600, height=600, bg='black')
 canvas.pack()
-
-line_one = canvas.create_line(0,0,0,0)
-line_two = canvas.create_line(0,0,0,0)
-line_three = canvas.create_line(0,0,0,0)
-line_four = canvas.create_line(0,0,0,0)
-polygon_bottom = canvas.create_polygon(0,0,0,0,0,0)
-polygon_top = canvas.create_polygon(0,0,0,0,0,0)
-polygon_left = canvas.create_polygon(0,0,0,0,0,0)
-polygon_right = canvas.create_polygon(0,0,0,0,0,0)
-
-canvas.bind("<B1-Motion>",shape)
-
+x = PerspectiveCube(canvas)
 tk.mainloop()
+
