f3ab4ab7f78bb63cac33dc408f480d0f03fb0efa
nyt <jonsspamaccout@gmail.com>
2018-07-02 23:17:11 -0700

    added multiple cubes

--- OnePointPerspectiveCube.py
+++ OnePointPerspectiveCube.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 import tkinter as tk
+from tkinter import ttk
+from itertools import cycle
 
+COLORS = cycle(['red', 'blue', 'green', 'magenta', 'yellow'])
 class PerspectiveCube:
     def __init__(self, canvas):
         self.canvas = canvas
         self.polygons = []
+        color = next(COLORS)
         for _ in range(4):
-            p = canvas.create_polygon(0,0,0,0,0,0, outline='red', fill='', width=4)
+            p = canvas.create_polygon(0,0,0,0,0,0, outline=color, fill='', width=4)
             self.canvas.tag_bind(p, "<B1-Motion>", self._on_clickndrag)
             self.polygons.append(p)
         self.canvas.tag_bind(self.polygons, "<B1-Motion>", self._on_clickndrag)
@@ -38,15 +42,26 @@ class PerspectiveCube:
         self.canvas.coords(self.polygons[2], x5, y5, x1, y1, x1+100, y1, x7, y7)
         self.canvas.coords(self.polygons[3], x2,y2,x1+100,y1,x7,y7, x4, y4)
 
+class PerspectiveCanvas(tk.Frame):
+    def __init__(self, master=None, **kwargs):
+        tk.Frame.__init__(self, master, bg='black', **kwargs)
+        label = tk.Label(self, text = "Click and Drag to See the Shape",background='black',foreground='white')
+        label.pack()
+        self.canvas = tk.Canvas(self, width=600, height=600, bg='black')
+        self.canvas.pack()
+        btn = ttk.Button(self, text='add polygon', command=self.add)
+        btn.place(relx=1, rely=0, anchor='ne')
+        self.cubes = []
+
+    def add(self):
+        self.cubes.append(PerspectiveCube(self.canvas))
+
 def main():
     root = tk.Tk()
     root.title("OnePointPerspectiveCube")
     root.configure(background='black')
-    label = tk.Label(text = "Click and Drag to See the Shape",background='black',foreground='white')
-    label.pack()
-    canvas = tk.Canvas(width=600, height=600, bg='black')
-    canvas.pack()
-    x = PerspectiveCube(canvas)
+    win = PerspectiveCanvas(root)
+    win.pack()
     root.mainloop()
 
 if __name__ == '__main__':
